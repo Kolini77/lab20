e2e2b008d9bc8369177c11587fb003107060a60b
edu-msk22-809s <edu-msk22-809s@MSK22EWS0923>
2023-03-15 15:00:38 +0300

    Добавьте файлы проекта.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,10 @@
+
+print("Table 1: ")
+for i in range(1,10):
+    for j in range(1,10):
+        print(i,"*",j,"=",i*j)
+ 
+print("Table 2: ")
+for i in range(1,10):
+    for j in range(1,10):
+        print(i,"+",j,"=",i+j)
