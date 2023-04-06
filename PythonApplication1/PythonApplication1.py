ecd25b861bbaeb0a72361f909152cd3c33f1835f
Wolee2974 <127952373+Wolee2974@users.noreply.github.com>
2023-04-06 15:42:43 +0300

    Update PythonApplication1.py

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,10 +1,10 @@
 
 print("Table 1: ")
 for i in range(1,10):
-    for j in range(1,10):
+    for j in range(1,10): # Выводим таблицу 1 с умножением 
         print(i,"*",j,"=",i*j)
  
 print("Table 2: ")
 for i in range(1,10):
-    for j in range(1,10):
+    for j in range(1,10): # Выводим таблицу 2 с вычеслением
         print(i,"+",j,"=",i+j)
